e52738d895758257781ed5d5e74d6b4d8c0c6bd7
imran <imufun2@gmail.com>
2021-02-04 01:13:13 +0600

    Player  Jump
    - Player jump
    - Add some ground image
    - Add Player Tag

---
+++ Assets/Script/BetterJump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public float fallMultiplayer = 4f;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplayer) * Time.deltaTime;
+        }
+    }
+}
--- Assets/Script/Kitty.cs
+++ Assets/Script/Kitty.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class Kitty : MonoBehaviour
 {
 
+    public float jumpForce;
     public float speed = 2f;
     private Animator anim;
 
     private float transalation;
 
+
+    public LayerMask whatIsGround;
+    public Transform groupPosition;
+    public bool Gronded = true;
+
+
+    private Rigidbody2D rb;
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump();
+        }
     }
 
     void FixedUpdate()
     {
+        
         TurnPlayer();
         transalation = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector3(transalation, 0f, 0f) * Time.deltaTime * speed);
-
+        // transform.Translate(new Vector3(transalation, 0f, 0f) * Time.deltaTime * speed);
+        rb.velocity = new Vector2(transalation * Time.deltaTime * speed, rb.velocity.y);
         if (transalation > 0 || transalation < 0)
         {
             anim.SetFloat("speed", 1);
@@ -47,5 +60,28 @@ public class Kitty : MonoBehaviour
             this.GetComponent<SpriteRenderer>().flipX = false;   
         }
 
+    }
+
+    bool isGrounded()
+    {
+        if (Physics2D.OverlapCircle(groupPosition.position, 0.3f, whatIsGround))
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
+
+    void jump()
+    {
+        if (!isGrounded())
+        {
+            return;
+        }
+        else
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
     }
 }
