632c616bc4817d403b8c63c16f860935b588f510
imran <imufun2@gmail.com>
2021-02-07 17:19:26 +0600

    changes

--- Assets/Script/Kitty.cs
+++ Assets/Script/Kitty.cs
@@ -133,15 +133,4 @@ public class Kitty : MonoBehaviour
     }
 
 
-
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if(collision.gameObject.tag == "Max"){
-    //        if (isDead)
-    //            return;
-
-    //        anim.SetTrigger("death");
-    //        isDead = true;
-    //    }
-    //}
 }
